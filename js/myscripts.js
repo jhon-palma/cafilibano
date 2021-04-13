603ac6b979d16996ef962498b1ebac7fada186e6
jhon1946 <jhon-palma@upc.edu.co>
2021-04-12 20:59:09 -0500

    Esal

---
+++ js/myscripts.js
@@ -0,0 +1,31 @@
+$(function () {
+  $('.venobox').venobox({
+    autoplay: true,
+    // bgcolor: 'rgba(122,193,79, 0.8)',
+    border: '5px',
+    // titleattr: 'Prueba',
+    titleattr  : 'data-title',
+    spinner: 'three-bounce',
+    spinColor: '#7ac14f',
+    // framewidth : '800px',
+    frameheight : 'auto',
+  });
+
+})
+
+$(function () {
+  $('.venobox-gallery').venobox({
+    autoplay: true,
+    // bgcolor: 'rgba(122,193,79, 0.8)',
+    border: '5px',
+    // titleattr: 'Prueba',
+    titleattr  : 'data-title',
+    spinner: 'three-bounce',
+    spinColor: '#7ac14f',
+    // framewidth : '800px',
+    frameheight : '500px',
+    share:[],
+
+  });
+
+})
